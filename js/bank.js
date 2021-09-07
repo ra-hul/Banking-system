c2d785741eb90e4e3fcb6f1bb91661467bced6aa
Rahul Paul <rahulshanto008@gmail.com>
2021-09-07 20:50:29 +0600

    banking-site added

--- js/bank.js
+++ js/bank.js
@@ -1,3 +1,14 @@
 document.getElementById('login-submit').addEventListener('click', function () {
-    console.log("clicked");
+    const emailField = document.getElementById('user-email');
+    const emailValue = emailField.value;
+
+    // get user password
+
+    const passwordField = document.getElementById("user-password");
+    const passwodValue = passwordField.value;
+
+    if (emailValue == "rahul@gmail.com" && passwodValue == "rahulpaul007") {
+
+        window.location.href = 'banking-site.html'
+    }
 })
