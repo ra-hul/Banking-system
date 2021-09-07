b8a18a89fe4d695adf0e4141783b9202ecf4cef6
Rahul Paul <rahulshanto008@gmail.com>
2021-09-08 01:20:03 +0600

    new

--- js/banking-site.js
+++ js/banking-site.js
@@ -1,5 +1,13 @@
 document.getElementById('deposit-button').addEventListener('click', function () {
-    console.log('clicked');
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmout = depositInput.value;
+    // console.log(depositAmout);
+
+    const depositTotal = document.getElementById('deposit-total');
+
+    depositTotal.innerText = depositAmout;
+
+    depositInput.value = '';
 })
 document.getElementById('withdraw-button').addEventListener('click', function () {
     console.log('clicked');
