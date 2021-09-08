cb7b2839f553a2f851173ae4c5f539130c41af13
Rahul Paul <rahulshanto008@gmail.com>
2021-09-08 15:50:29 +0600

    newBalance added

--- js/banking-site.js
+++ js/banking-site.js
@@ -17,8 +17,22 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     depositTotal.innerText = newDepositTotal;
 
 
+    // update acount balance
+
+    const balanceTotal = document.getElementById('balance-total');
+
+
+    const PreviousBalanceText = balanceTotal.innerText;
+    const PreviousBalanceAmount = parseFloat(PreviousBalanceText);
+
+    const TotalBalance = PreviousBalanceAmount + newDepositAmount;
+
+    balanceTotal.innerText = TotalBalance;
+
+
+
     depositInput.value = '';
 })
-document.getElementById('withdraw-button').addEventListener('click', function () {
-    console.log('clicked');
-})
+// document.getElementById('withdraw-button').addEventListener('click', function () {
+//     console.log('clicked');
+// })
